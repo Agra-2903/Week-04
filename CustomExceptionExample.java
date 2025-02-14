71cdf565567b2d6468e2eba06bdfc1c00f63099c
Kushagra Gupta <kushagragupta853@gmail.com>
2025-02-14 16:28:06 +0530

    [Kushagra] Add. Custom Exception Example program

---
+++ CustomExceptionExample.java
@@ -0,0 +1,26 @@
+package org.example;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomExceptionExample {
+    public static void main(String[] args) {
+        try {
+            validateAge(15);
+        } catch (InvalidAgeException e) {
+            System.out.println("Caught invalid age exception: " + e.getMessage());
+        }
+    }
+
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or older");
+        }
+        else {
+            System.out.println("Access granted!");
+        }
+    }
+}
